c0bbf571a2581892e29543d7545188d5e7b20552
bflanders <benjaminjflanders@gmail.com>
2018-12-10 21:06:36 -0500

    Removed lib in project/web. See project/lib/web

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,7 @@
 PROJECT='spending'
 USR='pi'
 # Project specific
-sudo cp -r /home/$USR/projects/$PROJECT/web/app/html/* /var/www/html/projects/$PROJECT
-
-sudo cp -r /home/$USR/projects/$PROJECT/web/app/js /var/www/html/projects/$PROJECT
-
-sudo cp -r /home/$USR/projects/$PROJECT/web/app/css /var/www/html/projects/$PROJECT
-
-sudo cp /home/$USR/projects/$PROJECT/web/cgi-bin/* /usr/lib/cgi-bin/$PROJECT
+sudo cp -r /home/$USR/projects/$PROJECT/web/app/* /var/www/html/projects/$PROJECT
 
 # Lib 
 sudo cp -r /home/$USR/projects/$PROJECT/lib/web/* /var/www/html/lib
